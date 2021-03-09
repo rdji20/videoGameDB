050fc88a9a0b7db2df8f068d6d71f85b49e6042d
Roberto Infante Lopez <infa@Robertos-MacBook-Pro.local>
2021-03-09 12:06:35 -0800

    Changes in views and name of document views

--- views_for_analysis.sql
+++ views_for_analysis.sql
@@ -10,4 +10,17 @@ AS
     JOIN tblORDER_GAME OG ON OG.OrderID = O.OrderID
     JOIN tblGAME G ON G.gameID = OG.gameID
     WHERE CT.CustomerTypeName = 'Mewtwo'
+    GO
+
+CREATE VIEW rdjiAmountOfGAMESperDeveloperPerMaturityRating
+AS
+    SELECT D.DeveloperID, D.Fname, D.Lname, COUNT(*) AS NumOfGames
+    FROM tblDEVELOPER D 
+    JOIN tblGameDeveloperRole GDR ON GDR.DevloperID = D.DeveloperID
+    JOIN tblGAME G ON G.gameID = GDR.gameID
+    JOIN Maturity_Rating MR ON MR.MaturityRatingID = G.MaturityRatingID
+    GROUP BY D.DeveloperID, D.Fname, D.Lname
+    GO
+
+
 
